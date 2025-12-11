edcdca0930c2b7072ea95b99416e7c9008edabf8
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2025-12-12 01:03:47 +0300

    Add create_payments.py

---
+++ users/management/commands/create_payments.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from users.models import User
+from materials.models import Course, Lesson
+from users.models import Payment
+
+class Command(BaseCommand):
+    help = "Создать тестовые платежи"
+
+    def handle(self, *args, **options):
+        # Получаем пользователя
+        user = User.objects.first()
+        if not user:
+            self.stdout.write(self.style.WARNING("Нет пользователей для привязки"))
+            return
+
+        # Получаем курс и урок
+        course = Course.objects.first()
+        lesson = Lesson.objects.first()
+
+        if not course and not lesson:
+            self.stdout.write(self.style.WARNING("Нет курса или урока для привязки"))
+            return
+
+        # Создаём платеж
+        payment = Payment.objects.create(
+            user=user,
+            paid_course=course,
+            paid_lesson=lesson,
+            amount=1000,
+            method="cash",
+            date=timezone.now()
+        )
+
+        self.stdout.write(self.style.SUCCESS(f"Платеж {payment.id} создан"))
