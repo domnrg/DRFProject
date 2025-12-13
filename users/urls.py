a7e8d9bae4e97057d29a091375660e78dd014af7
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2025-12-13 19:24:53 +0300

    Close each endpoint with authorization except registration and authorization

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .apps import UsersConfig
@@ -9,6 +10,6 @@ app_name = UsersConfig.name
 urlpatterns = [
     path('payments/', PaymentListAPIView.as_view(), name='payments_list'),
     path('register/', UserCreateAPIView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny, )), name='login'),
+    path('api/token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny, )), name='token_refresh'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from rest_framework.generics import ListAPIView, CreateAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from rest_framework.permissions import AllowAny
+
 from .models import Payment, User
 from .serializers import PaymentSerializer, UserSerializer
 
@@ -16,6 +18,7 @@ class PaymentListAPIView(ListAPIView):
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = (AllowAny, )
 
     def perform_create(self, serializer):
         user = serializer.save(is_active = True)
