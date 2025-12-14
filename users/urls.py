e2404fb9b0a6d0d5e36eeac8cdafb3bf0bd481a9
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2025-12-14 23:46:09 +0300

    Add urls for users

--- users/urls.py
+++ users/urls.py
@@ -4,12 +4,24 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
 from .apps import UsersConfig
-from .views import PaymentListAPIView, UserCreateAPIView
+
+from .views import (
+    PaymentListAPIView,
+    UserCreateAPIView,
+   UserRetrieveAPIView,
+    UserUpdateAPIView,
+    UserDestroyAPIView,
+)
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path("payments/", PaymentListAPIView.as_view(), name="payments_list"),
+    # пользователи
+    path("<int:pk>/", UserRetrieveAPIView.as_view(), name="user_detail"),
+    path("<int:pk>/update/", UserUpdateAPIView.as_view(), name="user_update"),
+    path("<int:pk>/delete/", UserDestroyAPIView.as_view(), name="user_delete"),
+
+    # регистрация и аутентификация
     path("register/", UserCreateAPIView.as_view(), name="register"),
     path(
         "login/",
@@ -21,4 +33,8 @@ urlpatterns = [
         TokenRefreshView.as_view(permission_classes=(AllowAny,)),
         name="token_refresh",
     ),
+
+    # платежи
+    path("payments/", PaymentListAPIView.as_view(), name="payments_list"),
 ]
+
